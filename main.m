5f48ab99c78229edfdf93a1b64ee02d42affd2ef
Asim H. Gazi <50971563+asimgazi24@users.noreply.github.com>
2020-05-07 17:27:01 -0500

    Adding 'main' script to run all other scripts

---
+++ main.m
@@ -0,0 +1,11 @@
+% This script runs all the constituent necessary scripts to reproduce
+% results using the example dataset included
+
+clear all
+
+% Prep biomarker series for modeling
+modelingPrep
+
+% State-space modeling
+ssestModeling
+
