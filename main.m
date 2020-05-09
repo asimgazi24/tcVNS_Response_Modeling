a2ed330358a0e46fdc09aa7508e22b3c22e6fa8b
Asim H. Gazi <50971563+asimgazi24@users.noreply.github.com>
2020-05-09 18:33:31 -0500

    includes simulated responses script

--- main.m
+++ main.m
@@ -9,7 +9,19 @@ modelingPrep
 % State-space modeling
 ssestModeling
 
-% Output Naive Predictor Fit Percentages
+% Compute Naive Predictor Fit Percentages
 naivePrediction_fitPercentages
+
+% Note that resampled experimental responses can be created by simply
+% loading the unfiltered, resampled, and normalized time series
+% produced and stored in the output from the ssestModeling script. From
+% there, averaging should hopefully be straightforward enough 
+% (accordingly, no example code is provided)
+
+% Thus, the final analysis step that readily available code would help in
+% elucidating is the simulated model response analysis
+
+% Simulate model responses
+simulate_modelResponses
 
 
